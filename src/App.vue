68e951a06aa9204441bb7a0772d12157f561238a
Joao Gomes <jg0112360@gmail.com>
2024-02-23 15:35:12 -0300

    feat: create project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Home from './components/Home.vue';
+</script>
+
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
+
+<style>
+#app {
+  background-color: rgb(255, 0, 0);
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="background">
+<h1>teste</h1>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
