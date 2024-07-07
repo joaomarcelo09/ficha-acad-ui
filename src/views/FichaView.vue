612a6efaa8cccebc1f834aacf332a6e104e90c81
Joao Gomes <jg0112360@gmail.com>
2024-07-07 14:58:46 -0300

    fix: update

--- src/views/FichaView.vue
+++ src/views/FichaView.vue
@@ -1,42 +1,43 @@
 <script setup lang="ts">
-import ListComponent from '@/components/ListComponent.vue';
-import { useFichaStore } from '@/stores/ficha';
-import { onMounted, ref } from 'vue';
+import ListComponent from '@/components/ListComponent.vue'
+import { useFichaStore } from '@/stores/ficha'
+import { onMounted, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter()
 const $fichaStore = useFichaStore()
 // const route = useRoute()
 
-
 const items = $fichaStore.fichas
 const columns = ref(['Nome', 'Data', 'Status'])
-const pages = ref([1,2,3])
+const pages = ref([1, 2, 3])
 const title = ref('Ficha')
 
 const openCreate = () => {
-    router.push('form')
+  router.push('form')
 }
 
 onMounted(async () => {
-    await $fichaStore.findAll()
+  await $fichaStore.findAll()
 })
-
-
-
 </script>
 
 <template>
-    <div class="body">
-        <ListComponent @open-create="openCreate" :title="title" :items="items" :columns="columns" :pagination="pages"/>
-    </div>
+  <div class="body">
+    <ListComponent
+      @open-create="openCreate"
+      :title="title"
+      :items="items"
+      :columns="columns"
+      :pagination="pages"
+    />
+  </div>
 </template>
 
 <style scoped>
 .body {
-
-    margin-top: 10rem;
-    margin-left: 1%;
-    width: 98%;
+  margin-top: 10rem;
+  margin-left: 1%;
+  width: 98%;
 }
 </style>
