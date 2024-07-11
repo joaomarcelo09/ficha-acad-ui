745ed2ab527cb57dc602d11cfed34938c1adc499
Joao Gomes <jg0112360@gmail.com>
2024-07-11 14:45:53 -0300

    fix: global css

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
-import './styles/colors.css'
-import './styles/fonts.css'
 
 import App from './App.vue'
 import router from './router'
