c34f6b1108f4e22038af71d031403cfac4aba86a
Joao Gomes <jg0112360@gmail.com>
2024-07-16 16:30:22 -0300

    fix: form component

--- src/components/form/FormComponent.vue
+++ src/components/form/FormComponent.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goBack = () => {
+  router.back()
+}
+
+defineProps<{
+  title?: string
+}>()
 </script>
 
 <template>
-      <div class="container-fluid page-title text-center"> Titulo</div>
+  <div class="d-flex flex-column align-items-center">
+    <div class="container-fluid page-title text-center">{{ title }}</div>
     <div class="card shadow">
-        <slot name="fields" ></slot>
+      <div class="card-body">
+        <div class="d-flex flex-wrap justify-content-between">
+          <slot name="fields"></slot>
+        </div>
+        <div class="d-flex justify-content-between mt-4">
+          <button type="button" class="btn btn-secondary">Voltar</button>
+          <button type="submit" class="btn btn-primary">Criar</button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
+.page-title {
+  height: 2rem;
+}
+.card {
+  width: 60%;
+}
 </style>
--- src/components/form/FormInput.vue
+++ src/components/form/FormInput.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-
 const props = defineProps<{
   label?: string
   placeholder?: string
   smalltext?: string
 }>()
-
 </script>
 
 <template>
-
-<div class="form-group col-3 mx-3 mt-2">
-    <label class="mb-2">{{ props.label }}</label>
-    <input type="text" class="form-control" :placeholder="props.placeholder">
-    <small class="form-text text-muted mt-2">{{ props.smalltext }}</small>
+  <div class="mx-3 mt-2 mb-3">
+    <label class="mb-1">{{ props.label }}</label>
+    <input type="text" class="form-control" :placeholder="props.placeholder" />
   </div>
-
 </template>
